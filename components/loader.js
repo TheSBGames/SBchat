aa0cef96a798326c236917d4a9afc513f08d2868
TheSBGames <thesdgams07@gmail.com>
2025-07-31 13:54:26 +0530

    Create loader.js

---
+++ components/loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div className="p-4 text-center text-gray-500 animate-pulse">
+      Thinking...
+    </div>
+  );
+}
